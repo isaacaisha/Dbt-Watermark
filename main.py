3a553abae1c12cd45ba4b120c2f360a82a729747
isaacaisha <medusadbt@gmail.com>
2023-07-22 05:14:31 +0200

    Prepare for Heroku deployment

---
+++ main.py
@@ -0,0 +1,184 @@
+from flask import Flask, render_template, request, session
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+BACKGROUND_COLOR = 'black'
+chosen_image_path = None
+logo_image_path = None
+modified_image_path = None
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('img/favicon.ico')
+
+
+@app.route('/')
+def home():
+    global chosen_image_path, logo_image_path, modified_image_path
+    chosen_image_path = "static/images/medusa1.png"
+    session.pop('logo_image_path', None)
+    session.pop('modified_image_path', None)
+    return render_template("index.html", chosen_image=chosen_image_path, logo_image=logo_image_path,
+                           BACKGROUND_COLOR=BACKGROUND_COLOR)
+
+
+@app.route('/choose_image', methods=['POST'])
+def choose_image():
+    global chosen_image_path, modified_image_path
+
+    if 'image_file' in request.files:
+        image_file = request.files['image_file']
+        if image_file.filename != '':
+            chosen_image_path = os.path.join('static', 'uploads', 'chosen_image.png')
+            image_file.save(chosen_image_path)
+            # Reset logo_image_path and modified_image_path when a new image is chosen
+            session.pop('logo_image_path', None)
+            session.pop('modified_image_path', None)
+            print("Image file received and saved successfully:", chosen_image_path)
+    else:
+        print("Image file not found in request.files")
+
+    # Check if the modified_image_path is already stored in the session
+    if 'modified_image_path' in session:
+        modified_image_path = session['modified_image_path']
+
+    return render_template("index.html", chosen_image=chosen_image_path, logo_image=logo_image_path,
+                           modified_image=modified_image_path, BACKGROUND_COLOR=BACKGROUND_COLOR)
+
+
+@app.route('/add_watermark_text', methods=['POST'])
+def add_watermark_text():
+    global chosen_image_path, modified_image_path
+
+    watermark_text = request.form['watermark_text']
+    if watermark_text:
+        img = Image.open(chosen_image_path)
+        draw = ImageDraw.Draw(img)
+
+        # Use the custom font from the "fonts" folder
+        font_path = 'static/fonts/arial.ttf'
+        font_size = 37
+
+        # Use try-except block to handle any errors related to font loading
+        try:
+            font = ImageFont.truetype(font_path, font_size)
+
+            # Get the bounding box for the text
+            text_bbox = draw.textbbox((0, 0), watermark_text, font=font)
+
+            # Calculate the width and height of the text bounding box
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+
+            # You can calculate the position of the text based on its size and image dimensions
+            x = img.width - text_width - 10
+            y = img.height - text_height - 10
+
+            # Add the watermark text in crimson color to the bottom right corner
+            draw.text((x, y), watermark_text, fill='crimson', font=font)
+
+            # Correct the path for the modified image
+            modified_image_filename = generate_modified_image_filename()
+            modified_image_path = os.path.join('static', 'uploads', modified_image_filename)
+
+            img.save(modified_image_path)
+            # Store modified_image_path in session for persistence
+            session['modified_image'] = modified_image_filename
+
+        except IOError:
+            # Use a default font if the custom font cannot be loaded
+            pass
+
+    return render_template("index.html", chosen_image=chosen_image_path, logo_image=logo_image_path,
+                           modified_image=modified_image_filename, BACKGROUND_COLOR=BACKGROUND_COLOR)
+
+
+def generate_modified_image_filename():
+    return 'modified_image.png'  # Use a fixed filename for the modified image
+
+
+@app.route('/add_watermark_logo', methods=['POST'])
+def add_watermark_logo():
+    global chosen_image_path, logo_image_path, modified_image_path
+
+    if 'logo_file' in request.files:
+        logo_file = request.files['logo_file']
+        if logo_file.filename != '':
+            logo_image_path = os.path.join('uploads', 'logo_image.png')
+            logo_file.save(logo_image_path)
+            # Store logo_image_path in session for persistence
+            session['logo_image_path'] = logo_image_path
+            # Call the function to handle the watermark logo
+            handle_watermark_logo()
+
+    # Check if the modified_image_path is already stored in the session
+    if 'modified_image_path' in session:
+        modified_image_path = session['modified_image_path']
+
+    return render_template("index.html", chosen_image=chosen_image_path, logo_image=logo_image_path,
+                           modified_image=modified_image_path, BACKGROUND_COLOR=BACKGROUND_COLOR)
+
+
+def handle_watermark_logo():
+    global chosen_image_path, logo_image_path, modified_image_path
+
+    if logo_image_path:
+        img = Image.open(chosen_image_path)
+        draw = ImageDraw.Draw(img)
+
+        # Load the watermark logo image
+        logo_img = Image.open(logo_image_path)
+
+        # Calculate the desired size for the watermark logo (e.g., 100x100 pixels)
+        desired_logo_size = (100, 100)
+
+        # Resize the watermark logo while maintaining its aspect ratio
+        logo_img.thumbnail(desired_logo_size)
+
+        # Calculate the position for the watermark logo at the top left corner
+        logo_x = 10
+        logo_y = 10
+
+        # Create a mask from the logo image's alpha channel
+        logo_mask = logo_img.convert('L').point(lambda x: 255 if x > 0 else 0)
+
+        # Paste the watermark logo on the modified image
+        img.paste(logo_img, (logo_x, logo_y), logo_mask)
+
+        # Correct the path for the modified image
+        modified_image_filename = generate_modified_image_filename()
+        modified_image_path = os.path.join('static', 'uploads', modified_image_filename)
+
+        img.save(modified_image_path)
+        # Store modified_image_path in session for persistence
+        session['modified_image_path'] = modified_image_path
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    global chosen_image_path, logo_image_path, modified_image_path
+
+    # Check if the modified_image_path is already stored in the session
+    if 'modified_image_path' in session:
+        modified_image_path = session['modified_image_path']
+        if modified_image_path:
+            new_image_path = os.path.join('uploads', 'final_image.jpg')
+            img = Image.open(modified_image_path)
+
+            # Convert the image to JPEG format before saving
+            img = img.convert('RGB')
+            img.save(new_image_path, format='JPEG')
+
+            # Reset modified_image_path after saving the final image
+            session.pop('modified_image_path', None)
+
+    return render_template("index.html", chosen_image=chosen_image_path, logo_image=logo_image_path,
+                           modified_image=modified_image_path, BACKGROUND_COLOR=BACKGROUND_COLOR)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
---
+++ watermark_app.py
@@ -0,0 +1,144 @@
+import io
+from tkinter import *
+from tkinter import filedialog, simpledialog
+from PIL import Image, ImageTk
+from ttkthemes import ThemedStyle
+
+BACKGROUND_COLOR = 'black'
+
+
+class WatermarkInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('WAT€RMARK')
+        self.window.config(padx=19, pady=19, bg=BACKGROUND_COLOR)
+
+        # Set the theme using ttkthemes
+        self.style = ThemedStyle(self.window)
+        self.style.set_theme('arc')  # You can choose a theme from the available ones
+
+        self.logo_image = None
+        self.logo_text = None
+
+        self.canvas = Canvas(height=433, width=379, highlightbackground='#B4CDE6', bg='black')
+        self.canvas.grid(column=1, row=2, pady=19)
+
+        true_img = PhotoImage(file='static/images/true.png')
+
+        # Store the chosen image as an attribute of the class
+        self.chosen_image = None
+
+        self.add_watermark_text_label = Label(fg='green', bg=BACKGROUND_COLOR, font=('Times', 27, 'italic'))
+        self.add_watermark_text_label.config(text=f"Add Watermark Text\n👇🏿👇🏿👇🏿")
+        self.add_watermark_text_label.grid(column=0, row=0)
+        self.true_btn_text = Button(image=true_img, highlightbackground='#B4CDE6', highlightthickness=0,
+                                    command=self.add_watermark_text)
+        self.true_btn_text.grid(column=0, row=1)
+
+        self.choose_img_label = Label(fg='yellow', bg=BACKGROUND_COLOR, font=('Times', 27, 'italic'))
+        self.choose_img_label.config(text=f"Click to choose a picture\n👇🏿👇🏿👇🏿")
+        self.choose_img_label.grid(column=1, row=0)
+        self.true_btn_img = Button(image=true_img, highlightbackground='#B4CDE6', highlightthickness=0,
+                                   command=self.choose_image)
+        self.true_btn_img.grid(column=1, row=1)
+
+        self.add_watermark_logo_label = Label(fg='red', bg=BACKGROUND_COLOR, font=('Times', 27, 'italic'))
+        self.add_watermark_logo_label.config(text=f"Add Watermark Logo\n👇🏿👇🏿👇🏿")
+        self.add_watermark_logo_label.grid(column=2, row=0)
+        self.true_btn_logo = Button(image=true_img, highlightbackground='#B4CDE6', highlightthickness=0,
+                                    command=self.add_watermark_logo)
+        self.true_btn_logo.grid(column=2, row=1)
+
+        self.save_btn = Button(text='Save ¡!¡', fg='green', highlightbackground=BACKGROUND_COLOR, command=self.save)
+        self.save_btn.grid(column=2, row=2)
+
+        self.be_good_label = Label(fg='#33FFD8', bg=BACKGROUND_COLOR, font=('Times', 27, 'bold'))
+        self.be_good_label.config(text=f'be good,\ndoing good,\nby acting good ¡!¡')
+        self.be_good_label.grid(column=0, row=2, padx=9)
+
+        self.window.mainloop()
+
+    def choose_image(self):
+        self.canvas.delete("watermark")
+        file_path = filedialog.askopenfilename(
+            title='Choose an image file',
+            filetypes=[('Image files', '*.png *.jpg *.jpeg *.gif *.bmp *.ico *.webp'),
+                       ('All files', '*.*')]
+        )
+        if file_path:
+            # Use PIL to open the image
+            pil_image = Image.open(file_path)
+
+            # Convert the PIL image to a PhotoImage
+            self.chosen_image = ImageTk.PhotoImage(pil_image)
+
+            # Place the PhotoImage on the canvas
+            self.canvas.create_image(self.canvas.winfo_reqwidth() / 2,
+                                     self.canvas.winfo_reqheight() / 2,
+                                     anchor="center", image=self.chosen_image)
+
+    def add_watermark_text(self):
+        if self.chosen_image:
+            # Remove any existing watermark from the canvas
+            self.canvas.delete("watermark")
+
+            # Get user input for the logo
+            self.logo_text = simpledialog.askstring("Enter Watermark Text", "Enter the Watermark text:")
+
+            # Place the watermark text at the right bottom corner of the chosen image
+            text_x = self.canvas.winfo_reqwidth() - 10
+            text_y = self.canvas.winfo_reqheight() - 10
+
+            # Add the new watermark text
+            self.canvas.create_text(text_x, text_y, anchor="se",
+                                    text=self.logo_text, fill='#9633FF',
+                                    font=('Arial', 19, 'bold'), tags="watermark")
+
+    def add_watermark_logo(self):
+        if self.chosen_image:
+            # Ask the user to choose a logo image
+            file_path = filedialog.askopenfilename(
+                title='Choose a logo image',
+                filetypes=[('Image files', '*.png *.jpg *.jpeg *.gif *.bmp *.ico *.webp'),
+                           ('All files', '*.*')]
+            )
+
+            if file_path:
+                # Use PIL to open the logo image
+                pil_logo = Image.open(file_path)
+
+                # Resize the logo image to a specific size (e.g., 100x100)
+                logo_size = (37, 37)
+                pil_logo = pil_logo.resize(logo_size)
+
+                # Convert the PIL image to a PhotoImage
+                self.logo_image = ImageTk.PhotoImage(pil_logo)
+
+                # Place the logo image in the top left corner of the chosen image
+                logo_x = 10
+                logo_y = 10
+
+                # Add the new watermark logo
+                self.canvas.create_image(logo_x, logo_y, anchor="nw", image=self.logo_image, tags="watermark")
+
+    def save(self):
+        if self.chosen_image:
+            # Convert the canvas content to an image with watermarks
+            modified_image = Image.open(io.BytesIO(self.canvas.postscript(colormode='color').encode('utf-8')))
+            # Convert to RGB mode if needed
+            modified_image = modified_image.convert('RGB')
+
+            # Ask the user to choose a save location
+            file_path = filedialog.asksaveasfilename(
+                defaultextension='.png',
+                filetypes=[('PNG files', '*.png'), ('JPEG files', '*.jpg *.jpeg'), ('All files', '*.*')]
+            )
+
+            if file_path:
+                # Save the modified image with watermarks as PNG with maximum quality
+                modified_image.save(file_path, compress_level=0)
+
+
+# Create an instance of the WatermarkInterface class and start the main loop
+watermark = WatermarkInterface()
